27828173b124d49a745ed831458b49d4f692bae1
Cosmin Catalin Sanda <cosmin@userreport.com>
2016-05-30 14:00:22 +0200

    Use literal variable

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ else
     echo "$WERCKER_PUBLISH_LAMBDA_FUNCTION_S3_ARTEFACT is not a valid S3 path"
     exit 1;
 fi
-if [ $(aws lambda list-functions | jq '.Functions[].FunctionName | select( . == "'$ENV{$1}'" )' | wc -c) -ne 0 ]
+if [ $(aws lambda list-functions | jq '.Functions[].FunctionName | select( . == "'$WERCKER_PUBLISH_LAMBDA_FUNCTION_FUNCTION_NAME'" )' | wc -c) -ne 0 ]
 then
-    aws lambda update-function-configuration --function-name $ENV{$1} --role "arn:aws:iam::$ENV{$3}:role/$ENV{$4}" --handler $ENV{$2} --timeout $ENV{$7} --memory-size $ENV{$8}
-    aws lambda update-function-code --function-name $ENV{$1} --s3-bucket $BUCKET --s3-key $KEY
+    aws lambda update-function-configuration --function-name $WERCKER_PUBLISH_LAMBDA_FUNCTION_FUNCTION_NAME --role "arn:aws:iam::$ENV{$3}:role/$ENV{$4}" --handler $ENV{$2} --timeout $ENV{$7} --memory-size $ENV{$8}
+    aws lambda update-function-code --function-name $WERCKER_PUBLISH_LAMBDA_FUNCTION_FUNCTION_NAME --s3-bucket $BUCKET --s3-key $KEY
 else
-    aws lambda create-function --function-name $ENV{$1} --runtime $ENV{$6} --role "arn:aws:iam::$ENV{$3}:role/$ENV{$4}" --handler $ENV{$2} --code S3Bucket=$BUCKET,S3Key=$KEY --timeout $ENV{$7} --memory-size $ENV{$8}
+    aws lambda create-function --function-name $WERCKER_PUBLISH_LAMBDA_FUNCTION_FUNCTION_NAME --runtime $ENV{$6} --role "arn:aws:iam::$ENV{$3}:role/$ENV{$4}" --handler $ENV{$2} --code S3Bucket=$BUCKET,S3Key=$KEY --timeout $ENV{$7} --memory-size $ENV{$8}
 fi
