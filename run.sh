1549bd737a20dd4264f54e13883df8facd3ba24a
Cosmin Catalin Sanda <cosmin@userreport.com>
2016-09-16 10:44:51 +0200

    Typo

--- run.sh
+++ run.sh
@@ -82,7 +82,7 @@ fi
 if [[ ! -z ${WERCKER_PUBLISH_LAMBDA_FUNCTION_EVENTS_SOURCE_ARN} ]]; then
     echo "Creating Lambda trigger."
     aws lambda create-event-source-mapping \
-        --event-source-arn ${$WERCKER_PUBLISH_LAMBDA_FUNCTION_EVENTS_SOURCE_ARN} \
+        --event-source-arn ${WERCKER_PUBLISH_LAMBDA_FUNCTION_EVENTS_SOURCE_ARN} \
         --function-name ${WERCKER_PUBLISH_LAMBDA_FUNCTION_FUNCTION_NAME} \
         --starting-position TRIM_HORIZON
 fi
